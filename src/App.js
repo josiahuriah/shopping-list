70912a6b80e8fbc39341bad801b593d7421d4830
Josh <josh.duncanson@gmail.com>
2023-10-18 05:37:45 -0400

    first commit 🚀

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  const [shoppingListItem, setShoppingListItem] = useState('')
+  const [shoppingList, setShoppingList] = useState([])
 
-export default App;
+  const handleSumbit = (e) => {
+    e.preventDefault()
+    setShoppingList(prevList =>[...prevList, shoppingListItem])
+    console.log(shoppingList)
+  }
+  const handleDeleteOption = (optionToRemove) => {
+    setShoppingList((prevList) => 
+      prevList.filter((option) => optionToRemove !== option )
+    )
+  }
+
+  return(
+    <div>
+      <h1>Shopping List</h1>
+      <form onSubmit={handleSumbit}>
+        <label htmlFor="shoppingListItem">Items To Buy</label>
+        <input type='text' required
+         value={shoppingListItem}
+         onChange={(e)=> setShoppingListItem(e.target.value)}
+         ></input>
+        <input type="submit" value="Submit"></input>
+      </form>
+      <div>
+        {shoppingList.map((item) => (
+        <div key={item}>
+          <p>{item}</p>
+          <button onClick={() => handleDeleteOption(item)}>Delete</button>
+        </div>))}
+      </div>
+    </div>
+  )
+}
